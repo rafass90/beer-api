f124f3dcc2b3277c85b12e30d3f506892530fa9c
Rafael <rafael9s9@hotmail.com>
2018-11-02 17:25:39 -0300

    Initial commit

---
+++ src/main/java/com/cyclic/beerapi/BeerApiApplication.java
@@ -0,0 +1,12 @@
+package com.cyclic.beerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerApiApplication.class, args);
+	}
+}
