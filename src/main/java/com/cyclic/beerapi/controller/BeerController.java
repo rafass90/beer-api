663db20d26b83dc6c6206dfd0bb7449c8a65e868
Rafael <rafael9s9@hotmail.com>
2018-11-03 14:44:05 -0300

    Removido endpoit de teste

--- src/main/java/com/cyclic/beerapi/controller/BeerController.java
+++ src/main/java/com/cyclic/beerapi/controller/BeerController.java
@@ -1,22 +1,24 @@
 package com.cyclic.beerapi.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cyclic.beerapi.spotify.SpotifyService;
+import com.cyclic.beerapi.database.BeerService;
+import com.cyclic.beerapi.vo.Beer;
 
 @RestController
 public class BeerController {
 
 	@Autowired
-	private SpotifyService spotifyService;
+	private BeerService beerService;
 	
-	@RequestMapping("/tests")
-	public ResponseEntity<String> test() {
-		
-		return new ResponseEntity<String>(spotifyService.getPlaylist_Sync(), HttpStatus.OK);
-	}
+	
 }
