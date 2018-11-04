ace499805b689c60d586e129f9073226432c8a4e
Rafael <rafael9s9@hotmail.com>
2018-11-04 05:02:44 -0200

    Refactoring Spotify connection

---
+++ src/main/java/com/cyclic/beerapi/domain/config/SpotifyConfiguration.java
@@ -0,0 +1,62 @@
+package com.cyclic.beerapi.domain.config;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+
+@Component
+@ConfigurationProperties(prefix = "spotify")
+public class SpotifyConfiguration{
+
+	private String clientID;
+
+	private String clientSecret;
+
+	private String redirectURI;
+
+	@Bean
+	public SpotifyApi spotifyApi() throws SpotifyWebApiException, IOException {
+		SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(this.clientID)
+				.setClientSecret(this.clientSecret)
+				.setRedirectUri(URI.create(this.redirectURI)).build();
+
+		ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
+		final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+		spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+		return spotifyApi;
+	}
+
+	public String getClientID() {
+		return clientID;
+	}
+
+	public void setClientID(String clientID) {
+		this.clientID = clientID;
+	}
+
+	public String getClientSecret() {
+		return clientSecret;
+	}
+
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
+
+	public String getRedirectURI() {
+		return redirectURI;
+	}
+
+	public void setRedirectURI(String redirectURI) {
+		this.redirectURI = redirectURI;
+	}
+	
+}
--- src/main/java/com/cyclic/beerapi/repository/spotify/SpotifyRepository.java
+++ src/main/java/com/cyclic/beerapi/repository/spotify/SpotifyRepository.java
@@ -1,7 +1,6 @@
 package com.cyclic.beerapi.repository.spotify;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.Arrays;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
@@ -9,39 +8,25 @@ import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.cyclic.beerapi.domain.config.SpotifyProperties;
 import com.neovisionaries.i18n.CountryCode;
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.model_objects.specification.Paging;
 import com.wrapper.spotify.model_objects.specification.PlaylistSimplified;
 import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
 import com.wrapper.spotify.model_objects.specification.Track;
-import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 
 @Service
 public class SpotifyRepository{
 	private final static Logger LOGGER = Logger.getLogger(SpotifyRepository.class.getName());
 
-	private SpotifyProperties spotifyProperties;
-
 	private SpotifyApi spotifyApi;
 
 	@Autowired
-	public SpotifyRepository(SpotifyProperties spotifyProperties) throws SpotifyWebApiException, IOException {
-		this.spotifyProperties = spotifyProperties;
-
-		spotifyApi = new SpotifyApi.Builder()
-				.setClientId(this.spotifyProperties.getClientID())
-				.setClientSecret(this.spotifyProperties.getClientSecret())
-				.setRedirectUri(URI.create(this.spotifyProperties.getRedirectURI()))
-				.build();
-
-		ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
-		final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
-		spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+	public SpotifyRepository(SpotifyApi spotifyApi) throws SpotifyWebApiException, IOException {
+		this.spotifyApi = spotifyApi;
 	}
+
 
 	public PlaylistSimplified getPlaylistByName(String name) {
 		PlaylistSimplified playlistSimplified = null;
